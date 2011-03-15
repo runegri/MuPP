a094ea685e0c9a4a6ecfb987cf6e384c9717c2af
runegri <rag@rag.no>
2011-03-15 23:25:39 +0100

    Refactored much of the code. Replaced the service reference with one created in Visual Studio for WP7. This means we can use the same service reference in both MonoTouch and WP7 projects.

---
+++ Src/SanntidWS/SanntidWS/AtB/BusStopListConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace AtB
+{
+    internal class BusStopListConverter : JsonConverterBase
+    {
+
+        public List<BusStop> GetBusStopsList(string busStopJson)
+        {
+
+            var jObject = JObject.Parse(busStopJson);
+
+            var stops = new List<BusStop>();
+            foreach (var stop in jObject.SelectToken("Fermate"))
+            {
+                var id = GetTokenValue(stop.SelectToken("cinFermata"));
+                var stopCode = GetTokenValue(stop.SelectToken("codAzNodo"));
+                var name = GetTokenValue(stop.SelectToken("descrizione"));
+                var lat = Convert.ToDouble(GetTokenValue(stop.SelectToken("lat")));
+                var lon = Convert.ToDouble(GetTokenValue(stop.SelectToken("lon")));
+
+                name = name.Replace(stopCode, "").Replace("()", "").Trim();
+
+                var location = ConvertWSCoordinates(lat, lon);
+
+                var busStop = new BusStop(id, stopCode, name, location);
+                stops.Add(busStop);
+            }
+
+            return stops;
+
+        }
+		
+		/// <summary>
+		/// Hack for converting coordinates from the web service to latitude/longtitude.
+		/// Accurate down to 1m-15m which is good enough for us here. 
+		/// </summary>
+		/// <param name="lat">
+		/// A <see cref="System.Double"/>
+		/// </param>
+		/// <param name="lon">
+		/// A <see cref="System.Double"/>
+		/// </param>
+		/// <returns>
+		/// A <see cref="GeographicCoordinate"/>
+		/// </returns>
+        private static GeographicCoordinate ConvertWSCoordinates(double lat, double lon)
+        {
+            var corrLat = (lat + 6541352)/248270.79;
+            var corrLon = lon/111319.4447;
+            return new GeographicCoordinate(corrLat, corrLon);
+        }
+
+    }
+}
---
+++ Src/SanntidWS/SanntidWS/AtB/StopTimeConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace AtB
+{
+    internal class StopTimeConverter:JsonConverterBase
+    {
+
+        public List<StopTime> GetStopTimes(string stopTimeJson)
+        {
+            var jObject = JObject.Parse(stopTimeJson);
+			
+			var times = jObject.SelectToken("Orari");
+			if (times != null && times.Any())
+			{
+				return times.Select(token => StopTimeFromToken(token)).ToList();
+			}
+			else
+			{
+				return new List<StopTime>();
+			}
+		}
+
+        private StopTime StopTimeFromToken(JToken token)
+        {
+            var routeNr = GetTokenValue(token.SelectToken("codAzLinea"));
+            var routeName =  GetTokenValue(token.SelectToken("descrizioneLinea"));
+            var routeTime = GetTokenValue(token.SelectToken("orario"));
+            var timeType =  GetTokenValue(token.SelectToken("statoPrevisione"));
+
+            return new StopTime(routeNr, routeName, routeTime, timeType);
+        }
+
+    }
+}
